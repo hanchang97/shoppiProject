230e674bf8e1b2c435a50eeb0ace63dbc1f9ece8
hanchang97 <nimok97@gmail.com>
2022-02-23 10:46:07 +0900

    refactor : 바인딩 제거

--- app/src/main/java/com/shoppi_han/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi_han/app/HomeFragment.kt
@@ -6,47 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.shoppi_han.app.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
-   /* private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!*/
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       /* _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view*/
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*binding.btnEnterProductDetail.setOnClickListener {
-            // 프래그먼트 매니저가 프래그먼트의 추가, 삭제, 교체를 담당!!!
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.add(R.id.container_main, ProductDetailFragment())
-            transaction.commit()
-        }*/
-
         val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
         button.setOnClickListener {
             val transaction = parentFragmentManager.beginTransaction()
-            //transaction.add(R.id.container_main, ProductDetailFragment()) // -> 질문  화면이 겹쳐보임!!
-            transaction.add(R.id.frameLayout, ProductDetailFragment()) // -> 질문  화면이 겹쳐보임!!
+            transaction.add(R.id.container_main, ProductDetailFragment()) // -> 질문  화면이 겹쳐보임!!
+
+            //transaction.add(R.id.frameLayout, ProductDetailFragment())
             //transaction.replace(R.id.container_main, ProductDetailFragment())
+
             transaction.commit()
         }
     }
 
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        //_binding = null
-    }
 }
--- app/src/main/java/com/shoppi_han/app/ProductDetailFragment.kt
+++ app/src/main/java/com/shoppi_han/app/ProductDetailFragment.kt
@@ -5,28 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.shoppi_han.app.databinding.FragmentHomeBinding
-import com.shoppi_han.app.databinding.FragmentProductDetailBinding
 
 class ProductDetailFragment : Fragment() {
-
-   /* private var _binding: FragmentProductDetailBinding? = null
-    private val binding get() = _binding!!*/
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        /*_binding = FragmentProductDetailBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view*/
         return inflater.inflate(R.layout.fragment_product_detail, container, false)
     }
 
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        //_binding = null
-    }
+
 }
