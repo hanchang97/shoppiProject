28776c92ae053826e80f5582bf5ece976dd17bb5
hanchang97 <nimok97@gmail.com>
2022-02-24 02:04:51 +0900

    feat : jetpack navigation 바텀내비게이션뷰 연결 후 프래그먼트 전환 구현

--- app/src/main/java/com/shoppi_han/app/MainActivity.kt
+++ app/src/main/java/com/shoppi_han/app/MainActivity.kt
@@ -2,6 +2,9 @@ package com.shoppi_han.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,16 @@ class MainActivity : AppCompatActivity() {
 
         /*supportFragmentManager.beginTransaction()
             .add(R.id.frameLayout, HomeFragment()).commit()*/
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
+
+        // 바텀 내비게이션과 fragment containerdml NavHostFragment를 연결
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let{
+            // null 이 아닌 경우
+            bottomNavigationView.setupWithNavController(it)
+        }
+
     }
 }
